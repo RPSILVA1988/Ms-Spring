da278879eb383d03e000bca3513ac91473c37147
RPSILVA1988 <comraniele@hotmail.com>
2021-08-31 22:13:35 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devaprendiz/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.devaprendiz.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devaprendiz.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
